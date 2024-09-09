c4ebdb56b451b5c6b8f33f399eabe9c44912b64d
Dan Keenan <dankeenan@shaw.ca>
2024-09-09 05:50:47 -0600

    For section 7 of Udemy course on docker-kubernetes

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
+import {act} from 'react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/I'm back bitches!/i);
+  expect(linkElement).toBeInTheDocument();
+});
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/I'm back bitches!/i);
   expect(linkElement).toBeInTheDocument();
 });
